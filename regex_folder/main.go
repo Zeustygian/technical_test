71f4321daeea962fc72923cc522dbcb7c4a48bbc
Lucas <lucas.gangnant@epitech.eu>
2022-05-23 18:28:52 +0400

    no more main functions

--- regex_folder/main.go
+++ regex_folder/main.go
@@ -11,6 +11,12 @@ func check_contain_month(arr_words string) bool {
 	if strings.Contains(arr_words, "euro/mois") == true {
 		return true
 	}
+	if strings.Contains(arr_words, "euros/mois") == true {
+		return true
+	}
+	if strings.Contains(arr_words, "EUR/mois") == true {
+		return true
+	}
 	if strings.Contains(arr_words, "/mois") == true {
 		return true
 	}
@@ -24,6 +30,12 @@ func check_contain_year(arr_words string) bool {
 	if strings.Contains(arr_words, "euro/an") == true {
 		return true
 	}
+	if strings.Contains(arr_words, "EUR/an") == true {
+		return true
+	}
+	if strings.Contains(arr_words, "euros/an") == true {
+		return true
+	}
 	if strings.Contains(arr_words, "/an") == true {
 		return true
 	}
@@ -34,13 +46,17 @@ func check_contain_year(arr_words string) bool {
 }
 
 func ExtractRent(content string) float32 {
+	if len(content) == 0 {
+		fmt.Println("Empty string")
+		os.Exit(84)
+	}
 	arr_words := strings.Split(content, " ")
 	for i := 0; i != len(arr_words); i++ {
 		if check_contain_year(arr_words[i]) == true {
 			for prev_word := i; prev_word != 0; prev_word-- {
 				price_year, err := strconv.Atoi(arr_words[prev_word])
 				if price_year > 0 {
-					fmt.Println(price_year)
+					fmt.Println("Price for a year is", price_year)
 					return float32(price_year)
 				}
 				_ = err
@@ -51,24 +67,12 @@ func ExtractRent(content string) float32 {
 				price_year, err := strconv.Atoi(arr_words[prev_word])
 				if price_year > 0 {
 					price_year *= 12
-					fmt.Println(price_year)
+					fmt.Println("Price for a year is", price_year)
 					return float32(price_year)
 				}
 				_ = err
 			}
 		}
 	}
-	return 84
-}
-
-func main() {
-	if len(os.Args) != 2 {
-		os.Exit(84)
-	}
-	content := os.Args[1]
-	if ExtractRent(content) == 84 {
-		os.Exit(84)
-	} else {
-		os.Exit(0)
-	}
+	return -1
 }
